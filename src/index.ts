8d507db30651a3c3e1e8028760d194df5a4b54a2
Volker Buzek <volker.buzek@js-soft.com>
2022-06-18 22:07:07 +0200

    chore: add defaults + template

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
+const deps = [
+    "@wdio/cli",
+    "@wdio/local-runner",
+    "@wdio/mocha-framework",
+    "@wdio/spec-reporter",
+    "wdio-chromedriver-service",
+    "wdio-ui5-service",
+    "chromedriver"
+]
+
 export function run() {
-  console.log("Hello World")
+    console.log("Hello World")
 }
