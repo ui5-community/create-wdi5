16c5e5374536c32f6ad270348d7e5149216a35b5
Volker Buzek <volker.buzek@js-soft.com>
2022-07-02 10:37:37 +0200

    chore: linting

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    root: true,
+    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        project: ["./tsconfig.json"]
+    },
+    plugins: ["@typescript-eslint"],
+    ignorePatterns: ["templates/**/*", ".eslintrc.js"]
+}
