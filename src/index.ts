e2057ddabba7142d1b05f789c8cbca4f747caa23
Volker Buzek <volker.buzek@js-soft.com>
2022-12-07 15:48:28 +0100

    feat(ts,js): unify support for all optional flags

--- src/index.ts
+++ src/index.ts
@@ -16,37 +16,59 @@ const DEV_DEPS = [
 
 const DEV_DEPS_TS = [...DEV_DEPS, "ts-node", "typescript"]
 let configPath = "./"
-let fullConfigPath: string | undefined
+let fullConfigPath: string
 let BASE_URL = "http://localhost:8080/index.html"
-let SPECS = "./webapp/test/**/*.test.js"
+let SPECS: string
+let relativeTestDir: string
+let absoluteTestDir: string
+let ts = false // whether we're working in TS land
 
 export async function run() {
     process.env.DEBUG && console.info("//> process.argv:")
     process.env.DEBUG && console.info(process.argv)
 
+    ts = process.argv.find((arg) => arg.includes("ts")) ? true : false
+
     if (process.argv.find((arg) => arg.includes("configPath"))) {
         const index = process.argv.findIndex((arg) => arg.includes("configPath")) + 1
         configPath = process.argv[index]
-        fullConfigPath = path.resolve(process.cwd(), configPath)
-        const rootDirExists = await fs.access(fullConfigPath).then(
-            () => true,
-            () => false
-        )
-        if (!rootDirExists) {
-            await fs.mkdir(fullConfigPath, { recursive: true })
-        }
+        configPath = configPath.endsWith(path.sep) ? configPath : configPath + path.sep
     }
 
-    process.argv.find((arg) => arg.includes("ts")) ? await initTS() : await initJS()
+    fullConfigPath = path.resolve(process.cwd(), configPath)
+    const rootDirExists = await fs.access(fullConfigPath).then(
+        () => true,
+        () => false
+    )
+    if (!rootDirExists) {
+        process.env.DEBUG && console.info(`//> created ${fullConfigPath}`)
+        await fs.mkdir(fullConfigPath, { recursive: true })
+    }
+
+    relativeTestDir = ts ? "./test" : "./webapp/test"
+    absoluteTestDir = path.resolve(process.cwd(), relativeTestDir)
+    const testDirExists = await fs.access(absoluteTestDir).then(
+        () => true,
+        () => false
+    )
+    if (!testDirExists) {
+        await fs.mkdir(absoluteTestDir, { recursive: true })
+    }
+
+    if (ts) {
+        SPECS = "./test/**/*.test.ts"
+        await initTS()
+    } else {
+        SPECS = "./webapp/test/**/*.test.js"
+        await initJS()
+    }
 }
 
 async function initJS() {
+    const wdioConf = path.resolve(fullConfigPath, "wdio.conf.js")
     console.log(gray(`≡> copying wdio.conf.js into "${configPath}"`))
-    await copyFile(
-        `${__dirname}/../templates/wdio.conf.js`,
-        `${fullConfigPath ? fullConfigPath : process.cwd()}/wdio.conf.js`
-    )
-    await _replacePlaceholder()
+    await copyFile(`${__dirname}/../templates/wdio.conf.js`, wdioConf)
+    await _replacePlaceholder(wdioConf)
     console.log(greenBright("👍 done!"))
 
     console.log(gray("≡> installing wdio + wdi5 and adding them as dev dependencies..."))
@@ -61,16 +83,13 @@ async function initJS() {
 }
 
 async function initTS() {
-    console.log(gray(`≡> copying tsconfig.json into ${configPath ? configPath : "./test/"}...`))
-    await copyFile(
-        `${__dirname}/../templates/test/tsconfig.json`,
-        `${fullConfigPath ? fullConfigPath : process.cwd()}/test/tsconfig.json`
-    )
+    console.log(gray(`≡> copying tsconfig.json into "${relativeTestDir}"...`))
+    await copyFile(`${__dirname}/../templates/test/tsconfig.json`, path.resolve(absoluteTestDir, "tsconfig.json"))
+
+    const wdioConf = path.resolve(fullConfigPath, "wdio.conf.ts")
     console.log(gray(`≡> copying wdio.conf.ts into "${configPath}"`))
-    await copyFile(
-        `${__dirname}/../templates/wdio.conf.ts`,
-        `${fullConfigPath ? fullConfigPath : process.cwd()}/wdio.conf.ts`
-    )
+    await copyFile(`${__dirname}/../templates/wdio.conf.ts`, wdioConf)
+    await _replacePlaceholder(wdioConf)
     console.log(greenBright("👍 done!"))
 
     console.log(gray("≡> installing wdio + wdi5 and adding them as dev dependencies..."))
@@ -79,7 +98,7 @@ async function initTS() {
 
     console.log(
         yellowBright(`\n≡> if your're using eslint, please add the "test"'s tsconfig.json to its' project setting:
-    "project": ["./tsconfig.json", "./test/tsconfig.json"]\n`)
+    "project": ["./tsconfig.json", "${relativeTestDir}/tsconfig.json"]\n`)
     )
 
     console.log(gray("≡> adding wdi5 start command to package.json..."))
@@ -89,7 +108,7 @@ async function initTS() {
     console.log(greenBright("👍 done!"))
 }
 
-async function _replacePlaceholder() {
+async function _replacePlaceholder(wdioConf: string) {
     if (process.argv.find((arg) => arg.includes("specs"))) {
         const index = process.argv.findIndex((arg) => arg.includes("specs")) + 1
         SPECS = process.argv[index]
@@ -100,9 +119,9 @@ async function _replacePlaceholder() {
         BASE_URL = process.argv[index]
     }
 
-    const data = await fs.readFile(`${fullConfigPath ? fullConfigPath : process.cwd()}/wdio.conf.js`)
+    const data = await fs.readFile(wdioConf)
     let fileString = data.toString()
     fileString = fileString.replace(/%specs%/g, SPECS)
     fileString = fileString.replace(/%baseUrl%/g, BASE_URL)
-    await fs.writeFile(`${fullConfigPath ? fullConfigPath : process.cwd()}/wdio.conf.js`, fileString)
+    await fs.writeFile(wdioConf, fileString)
 }
