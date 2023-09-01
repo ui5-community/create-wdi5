61d162f3e0eaad3487e1e8c517af3ef99a3f5a29
Volker Buzek <volker.buzek@js-soft.com>
2023-09-01 10:41:56 +0200

    refactor: jit compile

--- templates/wdio.conf.ts
+++ templates/wdio.conf.ts
@@ -32,13 +32,13 @@ export const config: wdi5Config = {
     //
 
     autoCompileOpts: {
-        autoCompile: true,
+        autoCompile: true
         // see https://github.com/TypeStrong/ts-node#cli-and-programmatic-options
         // for all available options
-        tsNodeOpts: {
-            transpileOnly: true,
-            project: "./test/tsconfig.json"
-        }
+        // tsNodeOpts: {
+        //     transpileOnly: true,
+        //     project: "./test/tsconfig.json"
+        // }
         // tsconfig-paths is only used if "tsConfigPathsOpts" are provided, if you
         // do please make sure "tsconfig-paths" is installed as dependency
         // tsConfigPathsOpts: {
